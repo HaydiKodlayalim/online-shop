7d67cd143c33b4b8605906f615efc23c0d107f62
tanertemel <taner.temel@coyoapp.com>
2020-09-26 13:43:13 +0200

    init project

---
+++ src/main/java/com/haydikodlayalim/shoppingapp/HkShoppingAppApplication.java
@@ -0,0 +1,13 @@
+package com.haydikodlayalim.shoppingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HkShoppingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HkShoppingAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/haydikodlayalim/shoppingapp/HkShoppingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haydikodlayalim.shoppingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HkShoppingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
