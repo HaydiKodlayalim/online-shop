d34a488e0dc5bed6eaa5eb7ac230d5282b2ee0c2
tanertemel <taner.temel@coyoapp.com>
2021-02-13 16:59:05 +0100

    product detail components and s3 minio

---
+++ web/src/components/product-detail/description.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="col-md-12 col-lg-12">
+    <div class="row">
+      Descripton
+    </div>
+    <hr>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "description"
+}
+</script>
+
+<style scoped>
+
+</style>
